afaea5730513f9b7914d9f53b5f76d702d31763a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-13 02:04:13 +0530

    ℹ️ simple route init

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use worker::*;
+use worker::{event, Context, Env, Request, Response, Result, Router};
 
 #[event(fetch)]
-pub async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    console_log!("Hello from Rust!");
-    Response::ok("Hello World!")
+pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let router = Router::new();
+    router
+        .get_async("/", |_, _| async move { Response::ok("200") })
+        .run(req, env)
+        .await
 }

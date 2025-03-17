d2bb6f7c6b344f93d06d8f629f7968598b78e14e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-18 00:30:31 +0530

    ☁️ aws storage updated

--- src/storage/aws.rs
+++ src/storage/aws.rs
@@ -1,32 +1,37 @@
 use crate::error::{MpcError, Result};
 use aws_sdk_s3::Client;
 
-pub async fn store(region: &str, data: &str) -> Result<String> {
-    let region_owned = region.to_owned();
-
-    let config = aws_config::from_env()
-        .region(aws_sdk_s3::config::Region::new(region_owned))
-        .load()
-        .await;
-
+pub async fn retrieve(pubkey: &str) -> Result<Vec<String>> {
+    let config = aws_config::load_from_env().await;
     let client = Client::new(&config);
     let bucket_name = std::env::var("AWS_BUCKET")
         .map_err(|_| MpcError::ConfigError("AWS_BUCKET not set".into()))?;
 
-    let object_key = format!("shares/{}.json", uuid::Uuid::new_v4());
+    let prefix = format!("shares/{}/", pubkey);
+    let mut shares = Vec::new();
 
-    client
-        .put_object()
-        .bucket(bucket_name)
-        .key(&object_key)
-        .body(data.as_bytes().to_owned().into())
+    let list = client
+        .list_objects_v2()
+        .bucket(&bucket_name)
+        .prefix(&prefix)
         .send()
-        .await
-        .map_err(
-            |e: aws_sdk_s3::error::SdkError<aws_sdk_s3::operation::put_object::PutObjectError>| {
-                MpcError::StorageError(e.to_string())
-            },
-        )?;
+        .await?;
+    for object in list.contents().unwrap_or_default() {
+        let key = object.key().unwrap();
+        let data = client
+            .get_object()
+            .bucket(&bucket_name)
+            .key(key)
+            .send()
+            .await?
+            .body
+            .collect()
+            .await
+            .map_err(|e| MpcError::StorageError(format!("Failed to collect object data: {}", e)))?;
 
-    Ok(object_key)
+        let share = String::from_utf8(data.to_vec())?;
+        shares.push(share);
+    }
+
+    Ok(shares)
 }
